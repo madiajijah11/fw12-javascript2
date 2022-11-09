0137f56bee209c6125edfe4c4290d4f5a625a42c
MexL <madiajijah7@gmail.com>
2022-11-09 17:41:47 +0800

    fix code explain

--- 3.js
+++ 3.js
@@ -31,7 +31,7 @@ const seleksiNilai = (nilaiAwal, nilaiAkhir, dataArray) => {
 	return result.length === 0 ? "Nilai tidak ditemukan" : result;
 };
 
-console.log(seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])); // [8, 10, 12, 14, 16, 18]
+console.log(seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8])); // [8, 14, 17]
 console.log(seleksiNilai(15, 3, [2, 25, 4, 14, 17, 30, 8])); // "Nilai akhir harus lebih besar dari nilai awal"
 console.log(seleksiNilai(5, 17, [2, 25, 4])); // "Jumlah angka dalam dataArray harus lebih dari 5"
 console.log(seleksiNilai(5, 17, [2, 25, 4, 1, 30, 18])); // "Nilai tidak ditemukan"
