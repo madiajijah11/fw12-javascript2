f948e8eaaa173bc9508dd760497305dc2262d3a8
MexL <madiajijah7@gmail.com>
2022-11-10 11:26:10 +0800

    change code explain

--- 2.js
+++ 2.js
@@ -20,10 +20,13 @@ const name = [
 ];
 
 const searchName = (keyword, limit, callback) => {
+	// filter the name converted to lowercase that contains the keyword
 	const result = name.filter((names) => names.toLowerCase().includes(keyword));
+	// run the callback function and pass the result and limit as the argument, and return it
 	return callback(result, limit);
 };
 
+// callback function that will return the result with the limit
 const sliceArray = (result, limit) => result.slice(0, limit);
 
-console.log(searchName("an", 3, sliceArray)); // ["Alexandra", "Amanda", "Angela"]
+console.log(searchName("bb", 3, sliceArray)); // ["Alexandra", "Amanda", "Angela"]
